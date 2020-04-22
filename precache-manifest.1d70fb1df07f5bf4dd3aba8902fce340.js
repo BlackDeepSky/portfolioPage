2dc478377705efdb02abbd1055b6b300586f10aa
AlexTizer <alexander.kalelov@gmail.com>
2020-04-22 13:51:35 +0300

    Updates

---
+++ precache-manifest.1d70fb1df07f5bf4dd3aba8902fce340.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f700ae764156fcd3ea357f031b9bab91",
+    "url": "/portfolioPage/index.html"
+  },
+  {
+    "revision": "8043200bc265fea194ab",
+    "url": "/portfolioPage/static/css/main.f17d891a.chunk.css"
+  },
+  {
+    "revision": "d789c6cae33720e4cdc8",
+    "url": "/portfolioPage/static/js/2.7521c9af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolioPage/static/js/2.7521c9af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8043200bc265fea194ab",
+    "url": "/portfolioPage/static/js/main.1f3c0879.chunk.js"
+  },
+  {
+    "revision": "73b75a2ef847e23a80e8",
+    "url": "/portfolioPage/static/js/runtime-main.cc267b30.js"
+  },
+  {
+    "revision": "c48c2ff745f757067dc1d3cdbce33620",
+    "url": "/portfolioPage/static/media/avatar.c48c2ff7.jpg"
+  },
+  {
+    "revision": "0240bd001b83569974e24c06f866f6a5",
+    "url": "/portfolioPage/static/media/redux.0240bd00.png"
+  }
+]);
